23843f5dd4fb09c2680e772b14f49576bceaa5eb
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-01-29 22:53:53 +0000

    Hit Ctrl+S to format code

--- Material/RenderPy-master/image.py
+++ Material/RenderPy-master/image.py
@@ -9,106 +9,119 @@
 
 import zlib, struct
 
+
 class Color(object):
-	""" A small class representing a 32-bit RGBA color."""
-	def __init__(self, r, g, b, a):
-		self.color = (r, g, b, a)
+    """A small class representing a 32-bit RGBA color."""
 
-	def r(self):
-		return self.color[0]
+    def __init__(self, r, g, b, a):
+        self.color = (r, g, b, a)
 
-	def g(self):
-		return self.color[1]
+    def r(self):
+        return self.color[0]
 
-	def b(self):
-		return self.color[2]
+    def g(self):
+        return self.color[1]
 
-	def a(self):
-		return self.color[3]
+    def b(self):
+        return self.color[2]
 
-	def getTuple(self):
-		return self.color
+    def a(self):
+        return self.color[3]
 
-	def getHexString(self):
-		return "0x%02X%02X%02X%02X" % self.color
+    def getTuple(self):
+        return self.color
 
-	def getByteString(self):
-		""" Pack the color as a C-style byte string."""
-		return struct.pack('>4B', self.color[0], self.color[1], self.color[2], self.color[3])
+    def getHexString(self):
+        return "0x%02X%02X%02X%02X" % self.color
 
-	def getAlphaBlend(self, destColor):
-		""" Alpha blend this color with the provided destination color."""
-		alpha = self.a() / 255
-		outR = int(self.r() * alpha) + int(destColor.r() * (1 - alpha))
-		outG = int(self.g() * alpha) + int(destColor.g() * (1 - alpha))
-		outB = int(self.b() * alpha) + int(destColor.b() * (1 - alpha))
-		outA = self.a() + int(destColor.a() * (1-alpha))
-		return Color(outR, outG, outB, outA)
+    def getByteString(self):
+        """Pack the color as a C-style byte string."""
+        return struct.pack(
+            ">4B", self.color[0], self.color[1], self.color[2], self.color[3]
+        )
+
+    def getAlphaBlend(self, destColor):
+        """Alpha blend this color with the provided destination color."""
+        alpha = self.a() / 255
+        outR = int(self.r() * alpha) + int(destColor.r() * (1 - alpha))
+        outG = int(self.g() * alpha) + int(destColor.g() * (1 - alpha))
+        outB = int(self.b() * alpha) + int(destColor.b() * (1 - alpha))
+        outA = self.a() + int(destColor.a() * (1 - alpha))
+        return Color(outR, outG, outB, outA)
+
 
 class Image(object):
-	""" An image class capable of generating and saving a PNG.
-		Attributes:
-			width: The width of the image
-			height: The height of the image
-			buffer: Representation of the image storing Color values for each pixel
-	"""
-	def __init__(self, width, height, color = Color(0, 0, 0, 255)):
-		""" Create the buffer, fill it with black pixels."""
-		self.width = width
-		self.height = height
+    """An image class capable of generating and saving a PNG.
+    Attributes:
+            width: The width of the image
+            height: The height of the image
+            buffer: Representation of the image storing Color values for each pixel
+    """
 
-		# Each row consists of a null byte followed by colors for each pixel
-		row = bytearray(1) + bytearray([color.r(), color.g(), color.b(), color.a()] * width)
-		self.buffer = row * height
+    def __init__(self, width, height, color=Color(0, 0, 0, 255)):
+        """Create the buffer, fill it with black pixels."""
+        self.width = width
+        self.height = height
 
-	def setPixel(self, x, y, color):
-		""" Set the color value for the pixel at (x, y)."""
-		if (x not in range(0, self.width)) or (y not in range (0, self.height)):
-			return
+        # Each row consists of a null byte followed by colors for each pixel
+        row = bytearray(1) + bytearray(
+            [color.r(), color.g(), color.b(), color.a()] * width
+        )
+        self.buffer = row * height
 
-		# Flip Y coordinate so that up is positive
-		flipY = (self.height - y - 1)
-		index = (flipY * self.width + x) * 4 + flipY
+    def setPixel(self, x, y, color):
+        """Set the color value for the pixel at (x, y)."""
+        if (x not in range(0, self.width)) or (y not in range(0, self.height)):
+            return
 
-		# Get the existing destination color
-		destColor = Color(*tuple(self.buffer[index + 1 : index + 5]))
+        # Flip Y coordinate so that up is positive
+        flipY = self.height - y - 1
+        index = (flipY * self.width + x) * 4 + flipY
 
-		# Blend the new color with the destination color
-		outColor = color.getAlphaBlend(destColor)
+        # Get the existing destination color
+        destColor = Color(*tuple(self.buffer[index + 1 : index + 5]))
 
-		# Set the new pixel colors in the buffer
-		self.buffer[index + 1 : index + 5] = outColor.getTuple()
+        # Blend the new color with the destination color
+        outColor = color.getAlphaBlend(destColor)
 
-	def saveAsPNG(self, filename = "render.png"):
-		""" Pack a new buffer formatted as a PNG, then save it to a file."""
-		print("Saving PNG...")
+        # Set the new pixel colors in the buffer
+        self.buffer[index + 1 : index + 5] = outColor.getTuple()
 
-		def makeChunk(chunkType, chunkData):
-			""" Pack data into standard PNG chunks. Chunks consist of:
-					- a 4-byte length 
-					- a 4-byte chunk type 
-					- the chunk data (compressed)
-					- a 4-byte cyclic redundancy check value (CRC)
-			"""
-			chunk = struct.pack(">I", len(chunkData)) + \
-					chunkType + \
-					chunkData +	\
-					struct.pack(">I", 0xFFFFFFFF & zlib.crc32(chunkType + chunkData))
+    def saveAsPNG(self, filename="render.png"):
+        """Pack a new buffer formatted as a PNG, then save it to a file."""
+        print("Saving PNG...")
 
-			return chunk
+        def makeChunk(chunkType, chunkData):
+            """Pack data into standard PNG chunks. Chunks consist of:
+            - a 4-byte length
+            - a 4-byte chunk type
+            - the chunk data (compressed)
+            - a 4-byte cyclic redundancy check value (CRC)
+            """
+            chunk = (
+                struct.pack(">I", len(chunkData))
+                + chunkType
+                + chunkData
+                + struct.pack(">I", 0xFFFFFFFF & zlib.crc32(chunkType + chunkData))
+            )
 
-		# Compose the PNG out of 3 chunks using the above function:
-		#	- IHDR: Header containing image size, color depth, existence of alpha channel etc. (See PNG spec)
-		#	- IDAT: Chunk containing the actual image data
-		#	- IEND: End-of-image chunk
+            return chunk
 
-		# Start with the universal PNG signature identifying the file as a PNG, then append chunks
-		packedData = b'\x89PNG\r\n\x1a\n' +	\
-					 makeChunk(b'IHDR', struct.pack(">2I5B", self.width, self.height, 8, 6, 0, 0, 0)) + \
-					 makeChunk(b'IDAT', zlib.compress(self.buffer, 9)) + \
-					 makeChunk(b'IEND', b'')
+        # Compose the PNG out of 3 chunks using the above function:
+        # 	- IHDR: Header containing image size, color depth, existence of alpha channel etc. (See PNG spec)
+        # 	- IDAT: Chunk containing the actual image data
+        # 	- IEND: End-of-image chunk
 
-		png = open(filename, 'wb')
-		png.write(packedData)
-		png.close()
-		
+        # Start with the universal PNG signature identifying the file as a PNG, then append chunks
+        packedData = (
+            b"\x89PNG\r\n\x1a\n"
+            + makeChunk(
+                b"IHDR", struct.pack(">2I5B", self.width, self.height, 8, 6, 0, 0, 0)
+            )
+            + makeChunk(b"IDAT", zlib.compress(self.buffer, 9))
+            + makeChunk(b"IEND", b"")
+        )
+
+        png = open(filename, "wb")
+        png.write(packedData)
+        png.close()
--- Material/RenderPy-master/vector.py
+++ Material/RenderPy-master/vector.py
@@ -1,9 +1,10 @@
 import math
 import numbers
 
+
 class Vector(object):
-    """ A vector with useful vector / matrix operations.
-    """
+    """A vector with useful vector / matrix operations."""
+
     def __init__(self, *args):
         if len(args) == 0:
             self.components = (0, 0)
@@ -12,7 +13,7 @@ class Vector(object):
 
     @property
     def x(self):
-        assert(len(self) >= 1)
+        assert len(self) >= 1
         return self.components[0]
 
     @x.setter
@@ -21,7 +22,7 @@ class Vector(object):
 
     @property
     def y(self):
-        assert(len(self) >= 2)
+        assert len(self) >= 2
         return self.components[1]
 
     @y.setter
@@ -30,7 +31,7 @@ class Vector(object):
 
     @property
     def z(self):
-        assert(len(self) >= 3)
+        assert len(self) >= 3
         return self.components[2]
 
     @z.setter
@@ -38,30 +39,33 @@ class Vector(object):
         self.components = (self.components[0], self.components[1], val)
 
     def norm(self):
-        """ Return the norm (magnitude) of this vector."""
+        """Return the norm (magnitude) of this vector."""
         squaredComponents = sum(math.pow(comp, 2) for comp in self)
         return math.sqrt(squaredComponents)
 
     def normalize(self):
-        """ Return a normalized unit vector from this vector."""
+        """Return a normalized unit vector from this vector."""
         magnitude = self.norm()
-        return Vector(*[comp/magnitude for comp in self])
+        return Vector(*[comp / magnitude for comp in self])
 
     def dot(self, other):
-        """ Return the dot product of this and another vector."""
+        """Return the dot product of this and another vector."""
         return sum(a * b for a, b in zip(self, other))
 
     def cross(self, other):
-        """ Return the cross product of this and another vector."""
+        """Return the cross product of this and another vector."""
         assert len(self) == len(other), "Vectors must be the same size."
         assert len(self) == 3, "Cross product only implemented for 3D vectors."
-        return Vector((self.y*other.z - self.z*other.y), (self.z*other.x - self.x*other.z), (self.x*other.y - self.y*other.x))
-
+        return Vector(
+            (self.y * other.z - self.z * other.y),
+            (self.z * other.x - self.x * other.z),
+            (self.x * other.y - self.y * other.x),
+        )
 
     # Overrides
     def __mul__(self, other):
-        """ If multiplied by another vector, return the dot product. 
-            If multiplied by a number, multiply each component by other.
+        """If multiplied by another vector, return the dot product.
+        If multiplied by a number, multiply each component by other.
         """
         if type(other) == type(self):
             return self.dot(other)
@@ -73,11 +77,11 @@ class Vector(object):
         if isinstance(other, numbers.Real):
             value = tuple(comp / other for comp in self)
             return Vector(*value)
-    
+
     def __add__(self, other):
         value = tuple(a + b for a, b in zip(self, other))
         return Vector(*value)
-    
+
     def __sub__(self, other):
         value = tuple(a - b for a, b in zip(self, other))
         return Vector(*value)
