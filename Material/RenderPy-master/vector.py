f88008bfa4b7b8ebf47dcefd6a81eee7192b69ee
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-02-29 23:24:26 +0000

    Finish question 2 part 4

--- Material/RenderPy-master/vector.py
+++ Material/RenderPy-master/vector.py
@@ -157,6 +157,25 @@ class Quaternion:
     def __iter__(self):
         return iter((self.w, self.x, self.y, self.z))
 
+    def __mul__(self, other):
+        """
+        --- Problem 2 Question 2 Part 4 ---
+
+        Calculates the product of this `Quaternion` and `other`
+        """
+
+        w1, x1, y1, z1 = self
+        w2, x2, y2, z2 = other
+
+        # fmt: off
+        return Quaternion(
+            w = (w1 * w2) - (x1 * x2) - (y1 * y2) - (z1 * z2),
+            x = (w1 * x2) + (x1 * w2) + (y1 * z2) - (z1 * y2),
+            y = (w1 * y2) - (x1 * z2) + (y1 * w2) + (z1 * x2),
+            z = (w1 * z2) + (x1 * y2) - (y1 * x2) + (z1 * w2),
+        )
+        # fmt: on
+
 
 class EulerAngles:
     """
