abad9741b70c714cc352b90335fb3a2a7a659269
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-03-01 22:37:09 +0000

    Put function-less logic into `render` and `main`

--- Material/RenderPy-master/render.py
+++ Material/RenderPy-master/render.py
@@ -10,14 +10,6 @@ NEAR_CLIP = 0.1
 
 width = 512
 height = 512
-image = Image(width, height, Color(255, 255, 255, 255))
-
-# Init z-buffer
-zBuffer = [-float("inf")] * width * height
-
-# Load the model
-model = Model("data/headset.obj")
-model.normalizeGeometry()
 
 
 # Set the camera back slightly from the origin so that the whole headset is
@@ -67,65 +59,83 @@ def getVertexNormal(vertIndex, faceNormalsByVertex):
     return normal / len(faceNormalsByVertex[vertIndex])
 
 
-# Calculate face normals
-faceNormals = {}
-for face in model.faces:
-    p0, p1, p2 = [model.vertices[i] for i in face]
-    faceNormal = (p2 - p0).cross(p1 - p0).normalize()
+def render(model: Model) -> None:
+    image = Image(width, height, Color(255, 255, 255, 255))
 
-    for i in face:
-        if not i in faceNormals:
-            faceNormals[i] = []
+    # Init z-buffer
+    zBuffer = [-float("inf")] * width * height
 
-        faceNormals[i].append(faceNormal)
+    # Calculate face normals
+    faceNormals = {}
+    for face in model.faces:
+        p0, p1, p2 = [model.vertices[i] for i in face]
+        faceNormal = (p2 - p0).cross(p1 - p0).normalize()
 
-# Calculate vertex normals
-vertexNormals = []
-for vertIndex in range(len(model.vertices)):
-    vertNorm = getVertexNormal(vertIndex, faceNormals)
-    vertexNormals.append(vertNorm)
+        for i in face:
+            if not i in faceNormals:
+                faceNormals[i] = []
 
-# Render the image iterating through faces
-for face in model.faces:
-    p0, p1, p2 = [model.vertices[i] for i in face]
-    n0, n1, n2 = [vertexNormals[i] for i in face]
+            faceNormals[i].append(faceNormal)
 
-    # Define the light direction
-    lightDir = Vector(0, 0, -1)
+    # Calculate vertex normals
+    vertexNormals = []
+    for vertIndex in range(len(model.vertices)):
+        vertNorm = getVertexNormal(vertIndex, faceNormals)
+        vertexNormals.append(vertNorm)
 
-    # Set to true if face should be culled
-    cull = False
+    # Render the image iterating through faces
+    for face in model.faces:
+        p0, p1, p2 = [model.vertices[i] for i in face]
+        n0, n1, n2 = [vertexNormals[i] for i in face]
 
-    # Transform vertices and calculate lighting intensity per vertex
-    transformedPoints = []
-    for p, n in zip([p0, p1, p2], [n0, n1, n2]):
-        intensity = n * lightDir
+        # Define the light direction
+        lightDir = Vector(0, 0, -1)
 
-        # Intensity < 0 means light is shining through the back of the face
-        # In this case, don't draw the face at all ("back-face culling")
-        if intensity < 0:
-            cull = True  # Back face culling is disabled in this version
+        # Set to true if face should be culled
+        cull = False
 
-        # coords = getOrthographicProjection(*p.xyz)
-        coords = getPerspectiveProjection(p)
-        if coords is None:
-            continue
+        # Transform vertices and calculate lighting intensity per vertex
+        transformedPoints = []
+        for p, n in zip([p0, p1, p2], [n0, n1, n2]):
+            intensity = n * lightDir
 
-        screenX, screenY = coords
+            # Intensity < 0 means light is shining through the back of the face
+            # In this case, don't draw the face at all ("back-face culling")
+            if intensity < 0:
+                cull = True  # Back face culling is disabled in this version
 
-        transformedPoints.append(
-            Point(
-                screenX,
-                screenY,
-                p.z,
-                Color(intensity * 255, intensity * 255, intensity * 255, 255),
+            # coords = getOrthographicProjection(*p.xyz)
+            coords = getPerspectiveProjection(p)
+            if coords is None:
+                continue
+
+            screenX, screenY = coords
+
+            transformedPoints.append(
+                Point(
+                    screenX,
+                    screenY,
+                    p.z,
+                    Color(intensity * 255, intensity * 255, intensity * 255, 255),
+                )
             )
-        )
 
-    # Don't draw triangles whose vertices have been cut off
-    if not cull and len(transformedPoints) == 3:
-        Triangle(
-            transformedPoints[0], transformedPoints[1], transformedPoints[2]
-        ).draw_faster(image, zBuffer)
+        # Don't draw triangles whose vertices have been cut off
+        if not cull and len(transformedPoints) == 3:
+            Triangle(
+                transformedPoints[0], transformedPoints[1], transformedPoints[2]
+            ).draw_faster(image, zBuffer)
 
-image.show()
+    image.show()
+
+
+def main() -> None:
+    # Load the model
+    model = Model("data/headset.obj")
+    model.normalizeGeometry()
+
+    render(model)
+
+
+if __name__ == "__main__":
+    main()
