35f0c095a30d8a475b1bdfb0fbb889d274aa652e
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-03-01 13:04:56 +0000

    Add suggested changes for homogeneous coords

--- Material/RenderPy-master/render.py
+++ Material/RenderPy-master/render.py
@@ -1,3 +1,4 @@
+
 from image import Image, Color
 from model import Model
 from shape import Point, Line, Triangle
@@ -14,30 +15,26 @@ zBuffer = [-float("inf")] * width * height
 model = Model("data/headset.obj")
 model.normalizeGeometry()
 
-
-def getOrthographicProjection(x, y, z):
-    # Convert vertex from world space to screen space
-    # by dropping the z-coordinate (Orthographic projection)
-    screenX = int((x + 1.0) * width / 2.0)
-    screenY = int((y + 1.0) * height / 2.0)
-
+def getOrthographicProjection(x, y, z, w=1):
+    # Convert vertex from world space (in homogeneous coordinates) to screen space
+    # by applying an orthographic projection (ignoring z and w for the conversion)
+    screenX = int((x / w + 1.0) * width / 2.0)
+    screenY = int((y / w + 1.0) * height / 2.0)
     return screenX, screenY
-
 
 def getVertexNormal(vertIndex, faceNormalsByVertex):
     # Compute vertex normals by averaging the normals of adjacent faces
-    normal = Vector(0, 0, 0)
+    normal = Vector(0, 0, 0, 0)  # Use homogeneous coordinates for normals
     for adjNormal in faceNormalsByVertex[vertIndex]:
         normal = normal + adjNormal
 
-    return normal / len(faceNormalsByVertex[vertIndex])
-
+    return normal.to_cartesian().normalize()  # Convert to Cartesian and normalize
 
 # Calculate face normals
 faceNormals = {}
 for face in model.faces:
-    p0, p1, p2 = [model.vertices[i] for i in face]
-    faceNormal = (p2 - p0).cross(p1 - p0).normalize()
+    p0, p1, p2 = [Vector(*model.vertices[i], 1) for i in face]  # Convert vertices to homogeneous coordinates
+    faceNormal = (p2 - p0).cross(p1 - p0).to_cartesian().normalize()  # Calculate and normalize the face normal
 
     for i in face:
         if not i in faceNormals:
@@ -53,11 +50,11 @@ for vertIndex in range(len(model.vertices)):
 
 # Render the image iterating through faces
 for face in model.faces:
-    p0, p1, p2 = [model.vertices[i] for i in face]
+    p0, p1, p2 = [Vector(*model.vertices[i], 1) for i in face]  # Convert vertices to homogeneous coordinates
     n0, n1, n2 = [vertexNormals[i] for i in face]
 
     # Define the light direction
-    lightDir = Vector(0, 0, -1)
+    lightDir = Vector(0, 0, -1, 0)  # Use homogeneous coordinates for the light direction
 
     # Set to true if face should be culled
     cull = False
@@ -65,19 +62,18 @@ for face in model.faces:
     # Transform vertices and calculate lighting intensity per vertex
     transformedPoints = []
     for p, n in zip([p0, p1, p2], [n0, n1, n2]):
-        intensity = n * lightDir
+        intensity = n.to_cartesian() * lightDir.to_cartesian()  # Convert normals back to Cartesian for lighting calculation
 
         # Intensity < 0 means light is shining through the back of the face
-        # In this case, don't draw the face at all ("back-face culling")
         if intensity < 0:
-            cull = True  # Back face culling is disabled in this version
+            cull = True
 
-        screenX, screenY = getOrthographicProjection(p.x, p.y, p.z)
+        screenX, screenY = getOrthographicProjection(p.x, p.y, p.z, p.w)
         transformedPoints.append(
             Point(
                 screenX,
                 screenY,
-                p.z,
+                p.z / p.w,  # Convert z to Cartesian for depth
                 Color(intensity * 255, intensity * 255, intensity * 255, 255),
             )
         )
--- Material/RenderPy-master/vector.py
+++ Material/RenderPy-master/vector.py
@@ -1,93 +1,104 @@
+
 import math
 import numbers
 
 
 class Vector(object):
-    """A vector with useful vector / matrix operations."""
+    """A vector with useful vector / matrix operations, adapted for homogeneous coordinates."""
 
     def __init__(self, *args):
+        # Adjust the constructor to handle an optional fourth component for homogeneous coordinates
         if len(args) == 0:
-            self.components = (0, 0)
+            self.components = (0, 0, 0, 1)  # Default to a point at the origin in homogeneous coordinates
+        elif len(args) == 3:
+            self.components = args + (1,)  # Treat as a point if three arguments are given
         else:
             self.components = args
 
     @property
     def x(self):
-        assert len(self) >= 1
         return self.components[0]
 
     @x.setter
     def x(self, val):
-        self.components = (val, self.components[1], self.components[2])
+        self.components = (val,) + self.components[1:]
 
     @property
     def y(self):
-        assert len(self) >= 2
         return self.components[1]
 
     @y.setter
     def y(self, val):
-        self.components = (self.components[0], val, self.components[2])
+        self.components = (self.components[0], val) + self.components[2:]
 
     @property
     def z(self):
-        assert len(self) >= 3
         return self.components[2]
 
     @z.setter
     def z(self, val):
-        self.components = (self.components[0], self.components[1], val)
+        self.components = self.components[:2] + (val,) + self.components[3:]
+
+    @property
+    def w(self):
+        return self.components[3]
+
+    @w.setter
+    def w(self, val):
+        self.components = self.components[:3] + (val,)
+
+    def to_cartesian(self):
+        """Converts the vector to Cartesian coordinates if it's in homogeneous coordinates."""
+        if self.w != 0:
+            return Vector(self.x / self.w, self.y / self.w, self.z / self.w)
+        return Vector(self.x, self.y, self.z)
 
     def norm(self):
-        """Return the norm (magnitude) of this vector."""
-        squaredComponents = sum(math.pow(comp, 2) for comp in self)
-        return math.sqrt(squaredComponents)
+        """Return the norm (magnitude) of this vector, ignoring the homogeneous component."""
+        squared_components = sum(math.pow(comp, 2) for comp in self.components[:3])
+        return math.sqrt(squared_components)
 
     def normalize(self):
-        """Return a normalized unit vector from this vector."""
+        """Return a normalized unit vector from this vector, maintaining the homogeneous component."""
         magnitude = self.norm()
-        return Vector(*[comp / magnitude for comp in self])
+        return Vector(*(comp / magnitude for comp in self.components[:3]) + (self.components[3],))
 
     def dot(self, other):
-        """Return the dot product of this and another vector."""
-        return sum(a * b for a, b in zip(self, other))
+        """Return the dot product of this and another vector, ignoring the homogeneous component."""
+        return sum(a * b for a, b in zip(self.components[:3], other.components[:3]))
 
     def cross(self, other):
-        """Return the cross product of this and another vector."""
-        assert len(self) == len(other), "Vectors must be the same size."
-        assert len(self) == 3, "Cross product only implemented for 3D vectors."
+        """Return the cross product of this and another vector, assuming 3D Cartesian coordinates."""
+        assert len(self) == 4 and len(other) == 4, "Vectors must be in homogeneous coordinates."
         return Vector(
-            (self.y * other.z - self.z * other.y),
-            (self.z * other.x - self.x * other.z),
-            (self.x * other.y - self.y * other.x),
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x,
+            1  # The result is a point in space
         )
 
-    # Overrides
+    # Overrides for arithmetic operations, adapted for homogeneous coordinates
     def __mul__(self, other):
-        """If multiplied by another vector, return the dot product.
-        If multiplied by a number, multiply each component by other.
-        """
-        if type(other) == type(self):
+        """If multiplied by another vector, return the dot product. If multiplied by a number, multiply each component by other."""
+        if isinstance(other, Vector):
             return self.dot(other)
         elif isinstance(other, numbers.Real):
-            product = tuple(comp * other for comp in self)
-            return Vector(*product)
+            return Vector(*(comp * other for comp in self.components))
 
     def __truediv__(self, other):
         if isinstance(other, numbers.Real):
-            value = tuple(comp / other for comp in self)
-            return Vector(*value)
+            return Vector(*(comp / other for comp in self.components))
 
     def __add__(self, other):
-        value = tuple(a + b for a, b in zip(self, other))
-        return Vector(*value)
+        # Addition should consider the vectors as points if they are in homogeneous coordinates
+        return Vector(*(a + b for a, b in zip(self.components, other.components)))
 
     def __sub__(self, other):
-        value = tuple(a - b for a, b in zip(self, other))
-        return Vector(*value)
+        # Subtraction should consider the vectors as points if they are in homogeneous coordinates
+        return Vector(*(a - b for a, b in zip(self.components, other.components)))
 
     def __len__(self):
         return len(self.components)
 
     def __iter__(self):
-        return self.components.__iter__()
+        return iter(self.components)
