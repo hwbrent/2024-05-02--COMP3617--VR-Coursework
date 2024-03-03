3f5504884da3f2f39b8f6f3f08e003f63dce7842
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-03-03 16:18:26 +0000

    Implement scalar multiplication of quaternion

--- Material/RenderPy-master/vector.py
+++ Material/RenderPy-master/vector.py
@@ -298,16 +298,28 @@ class Quaternion:
         """
 
         w1, x1, y1, z1 = self
-        w2, x2, y2, z2 = other
 
-        # fmt: off
-        return Quaternion(
-            w = (w1 * w2) - (x1 * x2) - (y1 * y2) - (z1 * z2),
-            x = (w1 * x2) + (x1 * w2) + (y1 * z2) - (z1 * y2),
-            y = (w1 * y2) - (x1 * z2) + (y1 * w2) + (z1 * x2),
-            z = (w1 * z2) + (x1 * y2) - (y1 * x2) + (z1 * w2),
-        )
-        # fmt: on
+        # If both are Quaternions:
+        if type(other) == type(self):
+            w2, x2, y2, z2 = other
+
+            # fmt: off
+            return Quaternion(
+                w = (w1 * w2) - (x1 * x2) - (y1 * y2) - (z1 * z2),
+                x = (w1 * x2) + (x1 * w2) + (y1 * z2) - (z1 * y2),
+                y = (w1 * y2) - (x1 * z2) + (y1 * w2) + (z1 * x2),
+                z = (w1 * z2) + (x1 * y2) - (y1 * x2) + (z1 * w2),
+            )
+            # fmt: on
+
+        # If 'other' is a scalar:
+        elif isinstance(other, numbers.Real):
+            return Quaternion(
+                w1 * other,
+                x1 * other,
+                y1 * other,
+                z1 * other,
+            )
 
     def __imul__(self, other: "Quaternion"):
         """
