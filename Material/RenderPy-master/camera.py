3a30a031cfc4c877572b8cdb0778a40ee3906008
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-03-20 18:19:15 +0000

    Add explanatory comment

--- Material/RenderPy-master/camera.py
+++ Material/RenderPy-master/camera.py
@@ -47,6 +47,12 @@ def lod_swap_needed(centre: Vector, prev_distance: float) -> bool:
     Returns a bool indicating whether the model with centre `centre` should
     be swapped out for one of a different level of detail
     """
+
+    # Basically we three "ranges" which correspond to the distances at which
+    # the respective model versions are used. We compare the distance before
+    # and after whatever transformation was just executed, and if they're not
+    # in the same range, we know the LoD swap of model versions is needed.
+
     new_distance = distance_to(centre)
 
     # `cat` = category
